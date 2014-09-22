62f48ebbc11e173288e3626fb669d0164318ec99
Rong Yan <rongyan@squareup.com>
2014-09-21 22:20:12 -0700

    Include additional changes for media processing

--- ToDoListTests/ToDoListTests.m
+++ ToDoListTests/ToDoListTests.m
@@ -7,6 +7,8 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <SenTestingKit/SenTestingKit.h>
+#import "TestToDoItem.h"
 
 @interface ToDoListTests : XCTestCase
 
@@ -18,17 +20,29 @@
 {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    NSLog(@"%s", __PRETTY_FUNCTION__);
 }
 
 - (void)tearDown
 {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+    NSLog(@"%s", __PRETTY_FUNCTION__);
     [super tearDown];
 }
 
 - (void)testExample
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    // XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    NSString *itemName = @"test1";
+    TestToDoItem *item = [[TestToDoItem alloc] init:itemName];
+    XCTAssertEqual(item.itemName, itemName, @"matched");
+}
+
+- (void)testNegExample
+{
+    NSString *itemName = @"test1";
+    TestToDoItem *item = [[TestToDoItem alloc] init:@"test2"];
+    XCTAssertNotEqual(item.itemName, itemName, @"not matched");
 }
 
 @end
